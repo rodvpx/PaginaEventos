207d71c8554fb607287ce3f1ff1db5ac6ad2447e
Rodrigo S <rodrigoip435@gmail.com>
2024-11-30 16:55:33 -0300

    feat: implementação de recarregamento da pagina, para que o fundo já tenha o gradiente

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,9 @@ const sections = document.querySelectorAll(".section");
 function handleScrollAnimations() {
   sections.forEach((section) => {
     const sectionTop = section.getBoundingClientRect().top;
-    const triggerPoint = window.innerHeight / 1.5; // Ajuste da altura da tela
+    const triggerPoint = window.innerHeight / 1.5;
 
-    // Adiciona a classe "active" para a transição de opacidade
+    // Adiciona a classe "active" quando a seção entra na tela
     if (sectionTop < triggerPoint) {
       section.classList.add("active");
     } else {
@@ -15,3 +15,6 @@ function handleScrollAnimations() {
 }
 
 window.addEventListener("scroll", handleScrollAnimations);
+
+// Garante que o conteúdo da primeira seção seja visível ao carregar a página
+window.addEventListener("load", handleScrollAnimations);
